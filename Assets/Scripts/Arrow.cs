5ce37798ef9276ec019a2b5a918b6f235f15e0f7
mahyar121 <mahyarhajibabaie@gmail.com>
2017-01-05 21:14:01 -0800

    fixed up the controllers

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Arrow : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D myRigidBody;
+    private Vector3 direction;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate ()
+    {
+        myRigidBody.velocity = direction * speed;
+	}
+
+    public void InitializeArrowDirection(Vector3 direction)
+    {
+        this.direction = direction;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
